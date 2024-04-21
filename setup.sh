15f2af5b681a9efdc07c401866631abe1c0a198e
shatteredsword <jdhbnoj7s@mozmail.com>
2024-04-20 22:29:19 -0700

    initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#cd Purpur/
+#./gradlew applyPatches
+#./gradlew build
+#./gradlew createReobfPaperclipJar
+#cd ..
+#ln -s Purpur/build/libs/purpur-paperclip-*.jar server.jar
+#cp minecraft.service /etc/systemd/system/minecraft.service
